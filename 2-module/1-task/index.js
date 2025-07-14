6c370e560206f15a03c2309c8f34f53a2888a8cd
sonia99-afk <miteneva.sofya.99@mail.ru>
2025-07-14 12:14:09 +0300

    3 lesson

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalary = 0;
+  for (let key in salaries) {
+    let value = salaries[key];
+    if (typeof value === 'number' && !isNaN(value) && value !== Infinity && value !== -Infinity) {
+      sumSalary += value;
+    }
+  }
+  return sumSalary;
 }
